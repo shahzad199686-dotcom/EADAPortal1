508a021a5ecaded3cbc810de532717191f058cb4
shehzad ali <sa298565@gmail.com>
2026-04-22 11:26:18 +0530

    Team Menu Option Added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,5 +21,16 @@ namespace EADAPortal.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult DownloadTeam()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files/team-details-demo.pdf");
+            if (!System.IO.File.Exists(path))
+            {
+                return NotFound();
+            }
+            var bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, "application/pdf", "Team-Details.pdf");
+        }
     }
 }
